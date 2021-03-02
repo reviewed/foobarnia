3bdb4ccb69f32160346b7de9f440e5d242d152c3
Vaughn <vaughn.weiss@gmail.com>
2021-03-01 19:29:12 -0700

    Mileage // Refactor nil mileage auto

--- mileage/auto_seeker_test.rb
+++ mileage/auto_seeker_test.rb
@@ -8,7 +8,6 @@ describe AutoSeeker do
       [2,'Blue',13999,25.0,'gas'],
       [3,'Teal',19000,27.0,'gas'],
       [4,'Red',14999,40.0,'diesel'],
-      [5,'Puce',29999,nil,'hamsters']
     ]
     @seeker = AutoSeeker.new data
   end
@@ -28,6 +27,13 @@ describe AutoSeeker do
   describe ".median_mileage " do
     it "calculates median mileage for all autos" do
       AutoSeeker.median_mileage(@seeker.autos).must_equal 26.0
+    end
+
+    it "calculates median mileage even with nil mileage values" do
+      no_mileage_auto = Auto.new [5,'Puce',29999,nil,'hamsters']
+      joined_array = @seeker.autos << no_mileage_auto
+
+      AutoSeeker.median_mileage(joined_array).must_equal 26.0
     end
   end
 end
