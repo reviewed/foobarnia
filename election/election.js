fb3972741598a57323e653c735c840a05f1327ac
MiriamPeskowitz <miriam5000@gmail.com>
2021-03-01 20:27:09 -0500

    Refactor to earlier 3 fe structure

--- election/election.js
+++ election/election.js
@@ -3,52 +3,58 @@
 // function than the tests account for. 
 
 exports.tally = function(votes) {
-	const winners = []
+	
 	//1. map with index
-	var mappedVotesWithIndex = votes.map((num, i) => {
+	const mappedVotesWithIndex = votes.map((num, i) => {
 	    return {index: i, value: num}
 		})
 
 	//2. sort
-	var sortedVotes = mappedVotesWithIndex.sort((a, b) => a.value - b.value).reverse()
+	const sortedVotes = mappedVotesWithIndex.sort((a, b) => a.value - b.value).reverse()
 
 	//3. check for Majority Win 
-	var topWinner = sortedVotes[0]
- 	var votesNeeded = (votes.reduce((a,b) => a + b) / 2
-   ) + 1
- 	var majWin = sortedVotes.some(vote=> vote.value >= votesNeeded) 
-	
-	if (majWin === true) {
-		winners = topWinner.index
+	const majorityWin = (votes, sortedVotes) => {
+		const topWinner = sortedVotes[0]
+	 	const votesNeeded = (votes.reduce((a,b) => a + b) / 2
+	   ) + 1
+	 	const majWin = sortedVotes.some(vote=> vote.value >= votesNeeded) 
 		
+		if (majWin === true) {
+			const majorityWinner = topWinner.index
+		}
+		return majorityWinner
 	}
 			//4. check for runOff scenarios
-	const noTie = (sortedVotes[2].value !== sortedVotes[3].value) 
+	const runOff = (votes, sortedVotes) => {
+		const noTie = (sortedVotes[2].value !== sortedVotes[3].value) 
 		// //4a. check for third-place tie
 		 //if true, use top Three winners, if false, go to runOff with Tie 
 		if (noTie === true) { 
-				const TopThreeWinners = [sortedVotes[0].index, sortedVotes[1].index, sortedVotes[2].index ]
-				return TopThreeWinners 
-			}
-
+				const TopThreeWinners = [sortedVotes[0].index, sortedVotes[1].index, sortedVotes[2].index ]	
+		}
+		return TopThreeWinners 
+	}
 		//5 check for how many are the same as sortedVotes[2].value
+	const runOffWithTie = (votes, sortedVotes) => {
 
-		var findTieArray = [...sortedVotes]
-				//remove top two
-		var topTwoWinners = findTieArray.splice(0,2)
-		//prep topTwoWinners for final array, need indices 
-		var topTwoWinnersIndices = topTwoWinners.map(t => t.index)
-
-		var tieValue = findTieArray[0].value //capture tied value to check how many there and result
+		const findTieArray = [...sortedVotes]
+		//remove top two; protect original array
+		const topTwoWinners = findTieArray.splice(0,2)
 		
+		//prep topTwoWinners for final allWinners array,  need indices 
+		const topTwoWinnersIndices = topTwoWinners.map(t => t.index)
+
+		//capture tied value to check how many there and result
+		const tieValue = findTieArray[0].value 
+
 		//create array with tied winners, and get their indices
-		var tieWinners = sortedVotes.filter(v => v.value === tieValue)
-		var tieWinnersIndices = tieWinners.map(t => t.index)
+		const tieWinners = sortedVotes.filter(v => v.value === tieValue)
+		const tieWinnersIndices = tieWinners.map(t => t.index)
 
 		//create final array of winners, and flatten to remove array of arrays 
-		//test doesn't like flatten -- concat instead. 
-		allWinners = [topTwoWinnersIndices.concat(tieWinnersIndices)]
+		//test doesn't like flat() -- use concat instead. 
+		const allWinners = [topTwoWinnersIndices.concat(tieWinnersIndices)]
 
-		return winners = 
+		return allWinners
 }
 
